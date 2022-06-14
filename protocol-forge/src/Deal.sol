b036de71fe57f914f48339fda12ed4f4fdc084cf
parseb <petra306@protonmail.com>
2022-06-14 15:36:47 +0300

    adds foundry with wrapped vyper support

---
+++ protocol-forge/src/Deal.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+/// @title CashCow.quest main
+/// @author parseb.eth
+/// @notice Deal functionality and structure. Fungible unit of a0z.vc protocol. 
+/// @custom:security contact: petra306@protonmail.com
+
+
+contract Deal is ERC721("a0z.vc", "A0Z.VC Seed Deal") {
+
+
+constructor();
+
+}
